f21385f0b5f33a644e5bf1ef8b6d16d0af9db396
Christian Zamora <christiantzq@gmail.com>
2022-12-28 12:03:34 -0500

    generated react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          React App test for Azure web app.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
